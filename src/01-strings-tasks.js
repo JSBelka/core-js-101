26ab0f6ec7e22c355c3cc45ca90bf49157da7d54
JSBelka <barbi-bar@mail.ru>
2024-09-02 21:21:30 +0300

    change string tasks

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -227,7 +227,7 @@ function getRectangleString(width, height) {
  *
  */
 function encodeToRot13(str) {
-  return str.replace(/[a-zA-Z]/g, char => {
+  return str.replace(/[a-zA-Z]/g, (char) => {
     const base = char <= 'Z' ? 65 : 97;
     return String.fromCharCode(base + ((char.charCodeAt(0) - base + 13) % 26));
   });
