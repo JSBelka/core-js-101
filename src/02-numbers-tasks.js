d5437058a952d7076ef6f3a9c13fecffed683f20
JSBelka <barbi-bar@mail.ru>
2024-09-03 21:59:23 +0300

    save changes in number tasks

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -228,7 +228,8 @@ function isPrime(n) {
   if (n < 2) {
     return false;
   }
-  for (let i = 2; i <= Math.sqrt(n); i = +1) {
+  // eslint-disable-next-line no-plusplus
+  for (let i = 2; i <= Math.sqrt(n); i++) {
     if (n % i === 0) {
       return false;
     }
