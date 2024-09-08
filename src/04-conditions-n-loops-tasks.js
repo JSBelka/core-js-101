33783ab72dbf0302a1d5dfb25f97fd9bbd055cd2
JSBelka <barbi-bar@mail.ru>
2024-09-08 19:52:49 +0300

    save changes in number tasks

--- src/04-conditions-n-loops-tasks.js
+++ src/04-conditions-n-loops-tasks.js
@@ -479,8 +479,19 @@ function getCommonDirectoryPath(pathes) {
  *                         [ 6 ]]
  *
  */
-function getMatrixProduct(/* m1, m2 */) {
-  throw new Error('Not implemented');
+function getMatrixProduct(m1, m2) {
+  const result = new Array(m1.length).fill(0).map(() => new Array(m2[0].length).fill(0));
+  // eslint-disable-next-line no-plusplus
+  for (let i = 0; i < m1.length; i++) {
+    // eslint-disable-next-line no-plusplus
+    for (let j = 0; j < m2[0].length; j++) {
+      // eslint-disable-next-line no-plusplus
+      for (let k = 0; k < m1[0].length; k++) {
+        result[i][j] += m1[i][k] * m2[k][j];
+      }
+    }
+  }
+  return result;
 }
 
 
